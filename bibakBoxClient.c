7418b246ac1b69e4e7cd32b47f88abd03b99ba4f
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-06-08 14:26:46 +0300

    detect , print changes and create log start only

--- bibakBoxClient.c
+++ bibakBoxClient.c
@@ -10,43 +10,94 @@
 int count = 0;
 
 void initialize_log_file(char* dir_name){
-    dir_info_bibak* curr_dir_info = malloc(sizeof(dir_info_bibak));
-    curr_dir_info->total_file_count = 0;
-    curr_dir_info->last_modified_time[0] = '\0';
-
     //Create log file path
     char* log_file_path = malloc(strlen(dir_name) + strlen("/log.txt") + 1);
     strcpy(log_file_path, dir_name);
     strcat(log_file_path, "/log.txt");
 
-    //Take the current directory info
-    search_dir(dir_name,curr_dir_info);
+    FILE *fp;
+    if ((fp = fopen(log_file_path,"r")) == NULL) {
+        //If log file doesn't exist then create
+        dir_info_bibak* curr_dir_info = malloc(sizeof(dir_info_bibak));
+        curr_dir_info->total_file_count = 0;
+        curr_dir_info->last_modified_time[0] = '\0';
+        curr_dir_info->files = malloc(0);
 
-    //Write log file
-    write_log_file(log_file_path,curr_dir_info);
+        //Write log file
+        write_log_file(log_file_path,curr_dir_info);
 
-    free(curr_dir_info);
+        free(curr_dir_info);
+    }
+    else
+        fclose(fp);
+    
     free(log_file_path);
 }
 
 request* compare_log_and_current_dir(dir_info_bibak* curr_dir_info, dir_info_bibak* log_dir_info){
     request* requests;
 
-    if(curr_dir_info->total_file_count != log_dir_info->total_file_count && curr_dir_info->last_modified_time != log_dir_info->last_modified_time){
-        for(int i = 0; i < curr_dir_info->total_file_count; i++){
-            for(int j = 0; j < log_dir_info->total_file_count; j++){
+    int isFound = 0;
+    printf("curr_dir_info->total_file_count : %d\n",curr_dir_info->total_file_count);
+    printf("log_dir_info->total_file_count  : %d\n",log_dir_info->total_file_count );
+    printf("curr_dir_info->last_modified_time  : %s\n",curr_dir_info->last_modified_time);
+    printf("log_dir_info->last_modified_time  : %s\n",log_dir_info->last_modified_time);
 
+    if(curr_dir_info->total_file_count != log_dir_info->total_file_count || strcmp(curr_dir_info->last_modified_time ,log_dir_info->last_modified_time) != 0){
+        
+        //Search current files in log file
+        for(int i = 0; i < curr_dir_info->total_file_count; i++){
+            isFound = 0;
+            for(int j = 0; j < log_dir_info->total_file_count; j++){
+                if( strcmp(curr_dir_info->files[i].name , log_dir_info->files[j].name) == 0 && 
+                    strcmp(curr_dir_info->files[i].path , log_dir_info->files[j].path) == 0){
+                    //File is found.
+                    isFound = 1;
+
+                    if(strcmp(curr_dir_info->files[i].last_modified_time,log_dir_info->files[j].last_modified_time) != 0){
+                        // The last modified time is different of log and current file so it has modified.
+                        // Post request to server to the update the file
+                        printf("UPDATE : %s\n",curr_dir_info->files[i].name);
+                    }
+
+                    break;
+                }
+            }
+
+            if(isFound == 0){
+                // File is added new , post request to server the upload the file
+                printf("UPLOAD : %s\n",curr_dir_info->files[i].name);
             }
         }
+
+        //Search log files in current files to determine deleted files
+        for(int i = 0; i < log_dir_info->total_file_count; i++){
+            isFound = 0;
+            for(int j = 0; j < curr_dir_info->total_file_count; j++){
+                if( strcmp(log_dir_info->files[i].name , curr_dir_info->files[j].name) == 0 && 
+                    strcmp(log_dir_info->files[i].path , curr_dir_info->files[j].path) == 0){
+                    //File is found.
+                    isFound = 1;
+                    break;
+                }
+            }
+
+            if(isFound == 0){
+                // File is deleted , post request to server the delete the file
+                printf("DELETE : %s\n",log_dir_info->files[i].name);
+            }
+        }
+
         requests = malloc(0);
     }
     else{
+        printf("There is no changes\n");
         requests = malloc(0);
     }
 
     
-    printf("count curr : %d\n",curr_dir_info->total_file_count);
-    printf("count log : %d\n",log_dir_info->total_file_count);
+    //printf("count curr : %d\n",curr_dir_info->total_file_count);
+    //printf("count log : %d\n",log_dir_info->total_file_count);
 
     return requests;
 }
